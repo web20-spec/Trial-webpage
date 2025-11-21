a4afe0c42b0994803388ad88675a26d9742f2169
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-11-21 12:18:35 -0800

    Upgradation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, {useId} from 'react'
+import React from 'react'
 
 const Input = function Input({
     label,
@@ -7,14 +7,10 @@ const Input = function Input({
     ref,
     ...props
 }){
-
-    const id = useId()
-
     return (
         <div className='w-full'>
             {label && <label
-            className='inline-block mb-1 pl-1'
-            htmlFor={id}>
+            className='inline-block mb-1 pl-1'>
                 {label}
                 </label>
                 }
@@ -24,7 +20,6 @@ const Input = function Input({
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
                 ref={ref}
                 {...props}
-                id={id}
                 />
         </div>
     )
